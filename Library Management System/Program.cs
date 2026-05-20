9d535e4458ae242c2fa28a3d6f81a069e528344d
Mohammed98112 <seaomn27@gmail.com>
2026-05-20 16:55:14 +0400

    Library project

---
+++ Library Management System/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata;
+
+namespace Library_Management_System
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        { 
+        
+        }
