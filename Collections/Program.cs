1a85a53f08d245b3970bb8f70dee7bc42435e146
Mohammed98112 <seaomn27@gmail.com>
2026-06-01 16:34:47 +0400

    structure

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -35,6 +35,34 @@
         }
         static void Main(string[] args)
         {
+                bool exit = false;
+                while (exit == false)
+                {
+
+                Console.WriteLine("=================================");
+                Console.WriteLine(" Arrays Practice Task Sheet");
+                Console.WriteLine("=================================");
+
+                Console.WriteLine("0. Temperature Log");
+                Console.WriteLine("1. Student Score Board");
+                Console.WriteLine("2. Product Price Finder)");
+                Console.WriteLine("3. Race Finish Times");
+                Console.WriteLine("4. Classroom Grade Report ");
+                Console.WriteLine("5. Warehouse Inventory Check");
+                Console.WriteLine("6. Library Book Shelf Scanner");
+                Console.WriteLine("7. Sales Performance Analyzer");
+                Console.WriteLine("8. Flight Seat Allocation Display");
+                Console.WriteLine("9. Hospital Patient Priority Queue");
+                Console.WriteLine("10. exit");
+                Console.WriteLine("=================================");
+
+
+                int choice;
+                    Console.WriteLine("enter your choice: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
                     //Temperature Log
                     case 0:
 
